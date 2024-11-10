1c8dbbadd7af788a4ccb280244e04afd2110f12a
pruthviraj Yadav <114584082+Pruthviraj002@users.noreply.github.com>
2024-11-10 21:02:55 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,29 @@
+const mongoose = require("mongoose")
+const express = require("express")
+require("dotenv/config")
+const BlogRoute = require("./Routes/blogRoute")
+const UserRoute = require("./Routes/userRoute")
+
+const app = express()
+app.use(express.json())
+
+app.get("/", (req, res) => {
+    res.send("Welcome to my blog")
+})
+
+app.use("/api/blog", BlogRoute)
+app.use("/api/user", UserRoute)
+
+app.listen(process.env.PORT)
+
+async function DB() {
+    try {
+        const res = await mongoose.connect(process.env.DB)
+        const data = res.STATES.connected
+        console.log(data);
+    } catch (error) {
+        console.log(error.message);
+    }
+}
+
+DB()
