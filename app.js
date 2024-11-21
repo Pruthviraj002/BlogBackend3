da211c0cd7bcd409677124d733ccf4349a8972b9
pruthviraj Yadav <114584082+Pruthviraj002@users.noreply.github.com>
2024-11-21 21:41:47 +0530

    add cors package

--- app.js
+++ app.js
@@ -3,8 +3,10 @@ const express = require("express")
 require("dotenv/config")
 const BlogRoute = require("./Routes/blogRoute")
 const UserRoute = require("./Routes/userRoute")
+const cors = require('cors')
 
 const app = express()
+app.use(cors())
 app.use(express.json())
 
 app.get("/", (req, res) => {
